0c576b62111f8c42b7aba140362530bd3ec38e1d
karthick sivakumar <karthicksivakumar97@gmail.com>
2024-07-03 21:56:10 +0530

    question method was refactored

--- quiz.py
+++ quiz.py
@@ -1,11 +1,16 @@
-answer = input("When was the first known use of the word 'quiz'? ")
-if answer == "1781":
-    print('Correct!')
-else:
-    print(f"The answer is '1781', not {answer!r}")
-    
-answer = input("Which built-in function can get information from the user? ")
-if answer == "input":
-    print("Correct!")
-else:
-    print(f"The answer is 'input', not {answer!r}")
+
+
+QUESTIONS = [
+    ("When was the first known use of the word 'quiz'", "1781"),
+    ("Which built-in function can get information from the user", "input"),
+    ("Which keyword do you use to loop over a given list of elements", "for" )
+]
+
+
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"THe answer is {correct_answer!r}, not {answer!r}")
+        
